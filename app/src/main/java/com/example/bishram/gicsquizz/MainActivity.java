77446ad2d12e1115c9f9a39bbe06f29584914031
bishram78 <bishram.munda78@gmail.com>
2018-05-14 21:15:07 +0530

    Update activity_home.xml and MainActivity.java Question #08 added with working java code

--- app/src/main/java/com/example/bishram/gicsquizz/MainActivity.java
+++ app/src/main/java/com/example/bishram/gicsquizz/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.ScrollView;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox option_5_a, option_5_b, option_5_c, option_5_d;
     private EditText editText_sixth_ans;
     private EditText editText_seventh_ans;
+    private EditText editText_eighth_ans;
     private RadioButton option_1_c;
     private RadioButton option_2_a;
     private RadioButton option_3_c;
@@ -39,12 +41,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         findAllViews();
+
+        checkFifthOption();
     }
 
     private void findAllViews() {
         btn_quiz_result = findViewById(R.id.id_btn_quiz_result);
         editText_sixth_ans = findViewById(R.id.id_et_ans_sixth);
         editText_seventh_ans = findViewById(R.id.id_et_ans_seventh);
+        editText_eighth_ans = findViewById(R.id.id_et_ans_eighth);
         option_1_c = findViewById(R.id.id_rb_opt_1_c);
         option_2_a = findViewById(R.id.id_rb_opt_2_a);
         option_3_c = findViewById(R.id.id_rb_opt_3_c);
@@ -64,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void quizResultButton(View view) {
         int quizScore = calcQuizScore();
-        String customMsg = "Your score is " + quizScore + "/35";
+        String customMsg = "Your score is " + quizScore + "/40";
         String thankYou = "Thanks for participation.";
         cToastShort(customMsg);
         cToastShort(thankYou);
@@ -78,8 +83,10 @@ public class MainActivity extends AppCompatActivity {
         int totalScored = 0;
         String ansSixth = "Imageview, Textview And Button";
         String ansSeventh = "Integrated Development Environment";
+        String ansEighth = "Kotlin";
         String getAnsSixth = editText_sixth_ans.getText().toString();
         String getAnsSeventh = editText_seventh_ans.getText().toString();
+        String getAnsEighth = editText_eighth_ans.getText().toString();
 
         if (option_1_c.isChecked()) {
             totalScored += 5;
@@ -97,14 +104,8 @@ public class MainActivity extends AppCompatActivity {
             totalScored += 5;
         }
 
-        else if (option_4_a.isChecked()&&option_4_b.isChecked()&&option_4_c.isChecked()&&option_4_d.isChecked())
-
         if (option_5_a.isChecked()&&option_5_b.isChecked()&&option_5_c.isChecked()) {
             totalScored += 5;
-        }
-
-        else if (option_5_a.isChecked()&&option_5_b.isChecked()&&option_5_c.isChecked()&&option_5_d.isChecked()) {
-            totalScored += 0;
         }
 
         if (getAnsSixth.equals(ansSixth)) {
@@ -114,7 +115,48 @@ public class MainActivity extends AppCompatActivity {
         if (getAnsSeventh.equals(ansSeventh)) {
             totalScored += 5;
         }
+
+        if (getAnsEighth.equals(ansEighth)) {
+            totalScored += 5;
+        }
         return totalScored;
+    }
+
+    private void checkFifthOption() {
+        option_5_a.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (option_5_a.isChecked()) {
+                    option_5_d.setChecked(false);
+                }
+            }
+        });
+        option_5_b.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (option_5_b.isChecked()) {
+                    option_5_d.setChecked(false);
+                }
+            }
+        });
+        option_5_c.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (option_5_c.isChecked()) {
+                    option_5_d.setChecked(false);
+                }
+            }
+        });
+        option_5_d.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (option_5_d.isChecked()) {
+                    option_5_a.setChecked(false);
+                    option_5_b.setChecked(false);
+                    option_5_c.setChecked(false);
+                }
+            }
+        });
     }
 
     /**
